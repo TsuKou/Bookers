bd293c6606675339f0f1d716a55eec95b93ccdb4
KouichiTsushima <147828512+TsuKou@users.noreply.github.com>
2023-11-08 12:11:29 +0000

    [finish20231108]課題2_途中経過

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,17 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
-    @book = Book.create
-    book = Book.create(book_params)
-    book.save
+    @book = Book.new(book_params)
+    @book.save
     redirect_to '/books/show'
   end
 
   def index
-    @book = Book
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -22,7 +23,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title,:body)
+    params.require(:book).permit(:title, :body)
   end
 
 end
