6874d1d654a21f498186a3a46e96c35c1ae79794
KouichiTsushima <147828512+TsuKou@users.noreply.github.com>
2023-11-11 13:53:25 +0000

    [Finsh Complete]2023/11/11 課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book.id)
     else
       @books = Book.all
-      render '/index'
+      render "index"
     end
   end
 
